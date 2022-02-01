7e65a3c4c00ff82c923bfd9747add73c70c60203
Lucas Luis Borges <lucasluisborges@outlook.com>
2022-02-01 14:24:23 -0300

    end Home page and start register page

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
+import React from 'react'
+import AppLoading from 'expo-app-loading'
 import { ThemeProvider } from 'styled-components'
 
 import {
@@ -10,22 +10,21 @@ import {
 } from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme'
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
     Poppins_700Bold
-  });
-  if(!fontsLoaded) {
+  })
+  if (!fontsLoaded) {
     return <AppLoading />
   }
 
-
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard /> 
+      <Register />
     </ThemeProvider>
-  ) 
+  )
 }
