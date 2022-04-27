6717b4041a3ac330fcb2f12c93e833680b0e3926
Lucas Luis Borges <lucasluisborges@outlook.com>
2022-04-27 16:14:16 -0300

    debug

--- src/__test__/hooks/auth.spec.tsx
+++ src/__test__/hooks/auth.spec.tsx
@@ -1,12 +1,13 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from '../../Hooks/auth';
 
-describe('Auth Hook', () => {
-  it('should be able to sign in with Google account existing', () => {
-    const { result } = renderHook(() => useAuth(), {
+describe('Auth Hook',() => {
+  it('should be able to sign in with Google account existing', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
     });
 
+    await waitForNextUpdate();
     console.log(result)
   });
 });
