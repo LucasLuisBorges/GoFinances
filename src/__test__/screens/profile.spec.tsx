c6c4f5157aab39f5c6ffca9a7b47a8d92ab9fc20
Lucas Luis Borges <lucasluisborges@outlook.com>
2022-04-27 13:16:30 -0300

    test component with styledComponent

--- src/__test__/screens/profile.spec.tsx
+++ src/__test__/screens/profile.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react-native'
 
 import { Profile } from '../../screens/Profile'
 
-describe('Profile screen', () => {
+describe('Profile Screen', () => {
   it('should have placeholder correctly in user name input', () => {
     const { getByPlaceholderText } = render(<Profile />);
 
