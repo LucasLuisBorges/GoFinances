a1c5bee2f8fecf73e7a8a426788f88033bffb59d
Lucas Luis Borges <lucasluisborges@outlook.com>
2022-04-29 11:40:54 -0300

    Mocks personalized

--- src/__test__/hooks/auth.spec.tsx
+++ src/__test__/hooks/auth.spec.tsx
@@ -1,39 +1,61 @@
-import { renderHook, act } from '@testing-library/react-hooks';
-import fetchMock from 'jest-fetch-mock';
+import { renderHook, act } from '@testing-library/react-hooks'
+import { mocked } from "jest-mock"
+import fetchMock from 'jest-fetch-mock'
+import { AuthProvider, useAuth } from '../../Hooks/auth'
+import { startAsync } from 'expo-auth-session'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { AuthProvider, useAuth } from '../../Hooks/auth';
-
-fetchMock.enableMocks();
+fetchMock.enableMocks()
 
 const userTest = {
   id: 'any_id',
   email: 'lucasluisborges1205@gmail.com',
   name: 'Lucas',
   photo: 'any_photo.png'
-};
+}
 
-jest.mock('expo-auth-session', () => {
-  return {
-    startAsync: () => ({
-      type: 'success',
-      params: {
-        access_token: 'any_token',
-      }
-    }),
-  }
-})
+jest.mock('expo-auth-session')
 
 describe('Auth Hook', () => {
+  beforeEach(async () => {
+    const userCollectionKey = "@gofinances:user";
+    await AsyncStorage.removeItem(userCollectionKey);
+  });
+
   it('should be able to sign in with Google account existing', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(userTest));
+    const googleMocked = mocked(startAsync as any)
+    googleMocked.mockReturnValueOnce({
+      type: 'success',
+      params: {
+        access_token: 'any_token'
+      }
+    })
+
+    fetchMock.mockResponseOnce(JSON.stringify(userTest))
 
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
-    });
+    })
 
-    await act(() => result.current.signInWithGoogle());
+    await act(() => result.current.signInWithGoogle())
 
-    expect(result.current.user.email)
-      .toBe(userTest.email);
-  });
-});
+    expect(result.current.user.email).toBe(userTest.email)
+  })
+
+  it('user should not connect if cancel authentication with Google', async () => {
+    const googleMocked = mocked(startAsync as any)
+    googleMocked.mockReturnValueOnce({
+      type: 'cancel'
+    })
+
+    fetchMock.mockResponseOnce(JSON.stringify(userTest))
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider
+    })
+
+    await act(() => result.current.signInWithGoogle())
+
+    expect(result.current.user).not.toHaveProperty('access_token')
+  })
+})
